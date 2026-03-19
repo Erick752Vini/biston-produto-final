1f451747994c76cfc4fe91298428b8f581bb551d
Erick752Vini <erickviniciusrocha80@gmail.com>
2026-03-19 17:21:08 -0300

    Add files via upload

---
+++ DML.sql
@@ -0,0 +1,41 @@
+INSERT INTO equipes (nome, codigo) VALUES
+("Mesa azul", "1"),
+("Mesa laranja", "2"),
+("Mesa amarela", "3"),
+("Mesa verde", "4"),
+("Mesa cinza", "5");
+
+INSERT INTO participantes (nome) VALUES 
+("Erick"),
+("Hugo"),
+("Sarah"),
+("Arthur"),
+("Jv");
+
+INSERT INTO equipes_participantes (id_participante, id_equipe) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1);
+
+INSERT INTO pistas (nome, id_equipe) VALUES
+("Autódromo de Interlagos", 1),
+("Circuito de Mônaco", 2),
+("Silverstone", 3),
+("Spa-Francorchamps", 4),
+("Monza", 5);
+
+INSERT INTO corridas (nome) VALUES 
+("Grande Prêmio do Brasil"),
+("Grande Prêmio de Mônaco"),
+("Grande Prêmio da Inglaterra"),
+("Grande Prêmio da Bélgica"),
+("Grande Prêmio da Itália");
+
+INSERT INTO dados_corridas (id_corrida, id_equipe, tempo_volta) VALUES 
+(1, 1, '00:01:14.500'),
+(1, 3, '00:01:13.200'),
+(2, 2, '00:01:16.400'), 
+(3, 4, '00:01:28.100'),
+(5, 5, '00:01:32.700');
---
+++ funcoes_idev4.sql
@@ -0,0 +1,76 @@
+DROP FUNCTION IF EXISTS total_de_voltas;
+DELIMITER //
+
+CREATE FUNCTION total_de_voltas (
+    _id_equipe INT,
+    _id_corrida INT
+)
+RETURNS INT
+READS SQL DATA -- DETERMINISTIC | READS SQL DATA | NO SQL
+BEGIN
+    DECLARE total INT; 
+    
+    SET total = (
+        SELECT COUNT(id)
+        FROM dados_corridas
+        WHERE id_equipe = _id_equipe
+          AND id_corrida = _id_corrida 
+    );
+    
+    RETURN total;
+END
+// DELIMITER ;
+
+-- 	-------------------------------------------------------------------------------------------------------------------------
+
+DROP FUNCTION IF EXISTS tempo_medio_volta;
+DELIMITER //
+CREATE FUNCTION  tempo_medio_volta(
+    _id_equipe INT,
+    _id_corrida INT
+)
+
+RETURNS TIME
+READS SQL DATA 
+BEGIN
+DECLARE media TIME;
+
+SET media = (
+        SELECT  AVG(tempo_volta)
+        FROM dados_corridas
+        WHERE id_corrida = _id_corrida
+        AND id_equipe = _id_equipe
+    );
+    
+    RETURN media;
+    END
+
+// DELIMITER ;
+
+SELECT tempo_medio_volta(1, 1);
+
+-- -------------------------------------------------------------------------------------------------------------------------
+
+DROP FUNCTION IF EXISTS melhor_volta;
+DELIMITER //
+
+CREATE FUNCTION melhor_volta(
+    _id_equipe INT,
+    _id_corrida INT
+)
+RETURNS TIME
+READS SQL DATA 
+BEGIN
+    DECLARE v_volta TIME;
+
+    SELECT MIN(tempo_volta) INTO v_volta
+    FROM dados_corridas  
+    WHERE id_corrida = _id_corrida
+    AND id_equipe = _id_equipe;
+    
+    RETURN v_volta;
+END
+// 
+DELIMITER ;
+
+SELECT melhor_volta(1, 1);
